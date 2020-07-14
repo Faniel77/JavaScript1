34bc2c0e5ddc9afe258e0031b8bffb49022ac9ef
Fanuel Asfaha <yemanefanuel15@gmail.com>
2020-07-14 17:50:54 +0200

    execute the hello world message in console.log

---
+++ Week1/homework/js-exercises/logHello.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('Hello, World'); //English
+console.log('Ciao, mondo'); //Italian
+console.log('Hej, världen'); // swedish
+console.log('Hola, mundo'); //Spanish
+console.log('Γειά σου Κόσμε'); //Greek
+console.log('مرحبا بالعالم'); //Arabic
+console.log('Hallo Wereld'); //Dutch
+console.log('Bonjour monde'); //French
+console.log('Hallo Welt'); //German
+console.log('ሰላም ልዑል'); //Amharic

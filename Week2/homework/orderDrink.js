06fffdb5a1727daac52fefd03c05c6889976dd34
Fanuel Asfaha <yemanefanuel15@gmail.com>
2020-07-20 22:59:06 +0200

    order drinks still pending

--- Week2/homework/orderDrink.js
+++ Week2/homework/orderDrink.js
@@ -1,16 +1,37 @@
 'use strict';
+// /* The code in this file will add the elements from the array "drinkTypes" to the array "drinkTray",
+// each element will be repeated twice.
+// when the "drinkTray.length = 6" the loop will stop "break" */
 
+// const drinkTray = [];
+// const drinkTypes = ['cola', 'lemonade', 'water'];
+
+const drinkTray = [];
 const drinkTypes = ['cola', 'lemonade', 'water'];
-let drinkTray = [];
-
-for (let i = 0; i < 5; i++) {
-  drinkTray = drinkTypes.push(i);
+for (i = 0; i < 5; i++) {
+  const drinkTray = drinkTypes.push(i);
+  console.log(drinkTray);
 }
-console.log(drinkTray);
-// for (let i = 0; i < 5; i++) {
-//   drinkTray = drinkTypes.map((drinktype) => drinktype + ',' + drinktype);
-//   console.log(drinkTray);
-// }
-// console.log('hi guys i brought  ' + drinkTray);
 
-// node orderDrink.js
+// for (let i = 0; i < 5; i++) {
+//   let index;
+//   if (drinkTray.length <= 1) {
+//     index = 0;
+//     drinkTray.push(drinkTypes[index]);
+//   } else if (drinkTray.length >= 2 && drinkTray.length <= 3) {
+//     index = 1;
+//     drinkTray.push(drinkTypes[index]);
+//   } else if (drinkTray.length >= 4 && drinkTray.length <= 5) {
+//     index = 2;
+//     drinkTray.push(drinkTypes[index]);
+//   } else {
+//     break;
+//   }
+// }
+// console.log(drinkTray);
+
+// // let drinks = '';
+// // for (const drink of drinkTray) {
+// //   drink === 'water' ? (drinks += `${drink}`) : (drinks += `${drink}, `);
+// // }
+// // console.log(`Hey guys, I brought a ${drinks}!`);

60d51cac3a36f17715dbcc5f17b6d1cdb20c841f
Fanuel Asfaha <yemanefanuel15@gmail.com>
2020-07-29 21:56:35 +0200

    dog years added

---
+++ Week3/homework/dogYears.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function calculateDogAge(puppyAge) {
+  let dogYears = puppyAge * 7;
+  console.log(`your doggie is ${dogYears} years old in dog years`);
+}
+calculateDogAge(1);
+calculateDogAge(2);
+calculateDogAge(3);
+
+// node dogYears.js
